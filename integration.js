3348128e2530354d81bb91b3ccf8bcda14cc87d7
Michael Salaverry <michaels@wix.com>
2016-12-01 21:23:33 +0200

    now it can arbitrarily integrate

--- integration.js
+++ integration.js
@@ -6,10 +6,10 @@ function trapezoid(x,deltax){
     return ((f(x+deltax)+f(x))*deltax)/2; //the trapezoid method is executed here for an arbitrary x and delta x and returns the area of the section
 }
 console.assert(trapezoid(2,.1)==2.1584,"integrate section wrong");
-function integrate(sections){ //loops over each section of the curve and returns the area of all the trapezoids
+function integrate(from,to,sections){ //loops over each section of the curve and returns the area of all the trapezoids
     var  out = 0;
-    let from = -1.5;
-    let to = 3;
+    //let from = -1.5;
+    //let to = 3;
     let sect = (to-from)/sections;
     for(let i = from;i<to;i+=sect){
         //console.log(i,sect,trapezoid(i,sect)); //see inside function
@@ -17,14 +17,13 @@ function integrate(sections){ //loops over each section of the curve and returns
     }
     return out; //area of all the trapezoids
 }
-console.assert(integrate(10)==58.3396875,"sectioning is broken");
-function howSmallIsEnough(answer){
+console.assert(integrate(-1.5,3,10)==58.3396875,"sectioning is broken");
+function howSmallIsEnough(from,to,answer){
     let x = 1;
     do{ 
          x++;
-         //console.log(x,integrate(x).toFixed(3)); //watch the accuracy change
-    } while(integrate(x).toFixed(3) != answer); //should be 57.6563 but only 10^-3 is important
-    return {trapezoidsNeeded : parseFloat(x), integral : integrate(x)}; //returns the number of trapezoids needed to accurately find the integral, and the calculated integral
+         //console.log(x,parseFloat(integrate(from,to,x).toFixed(3))); //watch the accuracy change
+    } while(integrate(from,to,x).toFixed(3) != answer); //should be 57.6563 but only 10^-3 is important
+    return {trapezoidsNeeded : parseFloat(x), integral : integrate(from,to,x)}; //returns the number of trapezoids needed to accurately find the integral, and the calculated integral
 }
-console.assert(howSmallIsEnough('57.656').trapezoidsNeeded == 525 && howSmallIsEnough('57.656').integral ==  57.656497959183746,"need to run more to be accurate enough");
-console.log(howSmallIsEnough('57.656'));
+console.assert(howSmallIsEnough(-1.5,3,'57.656').trapezoidsNeeded == 525 && howSmallIsEnough(-1.5,3,'57.656').integral ==  57.656497959183746,"need to run more to be accurate enough");
