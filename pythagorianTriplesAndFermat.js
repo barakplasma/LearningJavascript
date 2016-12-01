ed65211613301921c6bcff0af3458665e90b85d4
Michael Salaverry <michaels@wix.com>
2016-12-02 01:41:38 +0200

    pythag done

--- pythagorianTriplesAndFermat.js
+++ pythagorianTriplesAndFermat.js
@@ -20,21 +20,27 @@ function pythag(a,b,c,n){
 }
 console.assert(pythag(3,4,5,2)==true);
 function fermat(n,max){
-    var numTriples = 0 , a = 1 , b = 2, c = 3;
+    var numTriples = 0 , a=1,b=1,c = 1
     //print(3,4,5,2);
-    for(a,b,c;a<b&&b<c&&a<max;a++,b++,c++){
-        print(a,b,c,n);
-        console.log(a,b,c,n);
+    console.time("triploop");//this is the horribly inefficient triple loop
+    for(var a=1;a<max;a++){
+        for(var b=1;b<max;b++){
+            for(var c=1;c<max;c++){
+                print(a,b,c,n);
+            }
+        }
     }
     function print(a,b,c,n){
         if(pythag(a,b,c,n)){
+            if(gcd(a,b)==1&&a<b){
             console.log(`${a}^${n}+${b}^${n}=${c}^${n}`) //print the triple
-            console.log(`${power(n,a)}+${power(n,b)}=${power(n,c)}`) //print the triple
-            numTriples++;
+            console.log(`${power(n,a)}+${power(n,b)}=${power(n,c)}\n`) //print the triple
+            numTriples++;}
         }
     }
 
     return `We found ${numTriples} triples when checking n=${n} & max=${max}`;
 }
-console.log(fermat(2,100));//assert to http://www.tsm-resources.com/alists/trip.html
-console.log("done")
+console.log(fermat(2,1000));//assert to http://www.tsm-resources.com/alists/trip.html
+console.log("done");
+console.timeEnd("triploop");
