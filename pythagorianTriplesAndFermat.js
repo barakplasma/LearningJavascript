7f7f8a302038de1753d6a98ba9e00b7f6f0187b4
Michael Salaverry <michaels@wix.com>
2016-12-02 02:06:09 +0200

    done done

--- pythagorianTriplesAndFermat.js
+++ pythagorianTriplesAndFermat.js
@@ -9,18 +9,16 @@ function gcd(a,b){  // http://en.wikipedia.org/wiki/Euclidean_algorithm
 }//https://github.com/josdejong/mathjs/blob/master/lib/function/arithmetic/gcd.js
 console.assert(gcd(16,12)==4)
 function power(a,b){//a-th power of base 
-    for(a=a;a > 1;a--){
-        b *= b;
-    }
-    return b;
+    return Math.pow(b,a);
 }
 console.assert(power(2,4)==16);
+console.assert(power(3,4)==Math.pow(4,3),"mod to math.pow failed");
 function pythag(a,b,c,n){
     return power(n,a)+power(n,b)==power(n,c); //returns true if triple
 }
 console.assert(pythag(3,4,5,2)==true);
 function fermat(n,max){
-    var numTriples = 0 , a=1,b=1,c = 1
+    var numTriples = 0 , a=1,b=1,c = 1;
     //print(3,4,5,2);
     console.time("triploop");//this is the horribly inefficient triple loop
     for(var a=1;a<max;a++){
@@ -38,9 +36,10 @@ function fermat(n,max){
             numTriples++;}
         }
     }
-
-    return `We found ${numTriples} triples when checking n=${n} & max=${max}`;
+    if(numTriples!=0){return `We found ${numTriples} triples when checking n=${n} & max=${max}`}
+    else{return `We found no triples for n=${n}`};
 }
-console.log(fermat(2,1000));//assert to http://www.tsm-resources.com/alists/trip.html
+console.log(`Lets Calculate Pythagorian triples to prove Fermat’s last theorem\n`)
+console.log(fermat(2,100));//assert to http://www.tsm-resources.com/alists/trip.html
 console.log("done");
 console.timeEnd("triploop");
