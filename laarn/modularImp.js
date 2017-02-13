dce4aadada9b6c3861117277521bca5ff1b676d7
Michael Salaverry <michaels@wix.com>
2017-02-13 19:12:44 +0200

    it's a dirrr

--- laarn/modularImp.js
+++ laarn/modularImp.js
@@ -1,1 +1,14 @@
 var exp = require('./moduleExp.js');
+var fs = require('fs');
+//console.log(process.argv[2]);
+var path = process.argv[2];
+var fileExt = process.argv[3];
+console.log('path: ',path);
+
+fs.readdir(path,(err,dir)=>{
+    //console.log('dir:', dir);
+    exp(path,fileExt,pushToConsole);
+});
+function pushToConsole(out){
+    console.log(out);
+}
--- laarn/moduleExp.js
+++ laarn/moduleExp.js
@@ -2,10 +2,11 @@ var fs = require('fs');
 var pa = require('path');
 
 module.exports = function (dir,fileExt,callback){
-    readDir(fs.readFile(dir),fileExt);
-
-    function readDir(list,fileExt){
-        var out = list.filter((file)=>pa.extname(file)==fileExt);
-    }
+    var list = fs.readFile(dir);
+    
+    var out //= fs.readFile(dir);
+    //function readDir(list,fileExt){}
+    out = list.filter((file)=>pa.extname(file)==fileExt);
+    
     callback(null,out);
 }
