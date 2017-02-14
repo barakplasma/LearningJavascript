ff043490fe26a716a8bb2b83bd99dd827c53ed01
Michael Salaverry <michaels@wix.com>
2017-02-14 15:47:27 +0200

    language changes

--- laarn/getANS.js
+++ laarn/getANS.js
@@ -41,10 +41,10 @@ function checkA(err, Arecord) {
     callback(null, responseJSON);
   } else {
     if(/(216\.139\.213\.144)|(23\.236\.62\.147)|(216\.185\.152\.1((4[4-9])|(5[0-9])))|(104\.197\.84\.185)/.test(Arecord)){ //tests https://regex101.com/r/nm44aQ/1
-      responseJSON.response = "Your A record is set correctly.<br>";
+      responseJSON.response = "The domain's A record is set correctly.<br>";
       dns.resolve(event.result, 'NS', checkNS);
     } else {
-      responseJSON.response = `Your A record is ${Arecord}.<br>Check what your settings should be <a href="https://domain-troubleshooter.wix.com/ns-widget">here.</a><br>You can read more about how to <a href="https://support.wix.com/en/article/adding-or-updating-a-records-in-your-wix-account">change your A record through Wix</a> if you are using Wix nameservers. If you connected your domain via pointing, you will need to change the domain's A record through your domain registrar. `
+      responseJSON.response = `The domain's A record is ${Arecord}.<br>Check what your settings should be <a href="https://domain-troubleshooter.wix.com/ns-widget">here.</a><br>You can read more about how to <a href="https://support.wix.com/en/article/adding-or-updating-a-records-in-your-wix-account">change your A record through Wix</a> if you are using Wix nameservers. If you connected your domain via pointing, you will need to change the domain's A record through your domain registrar. `
       dns.resolve(event.result, 'NS', checkNS);
     }
   }
@@ -60,7 +60,7 @@ function checkNS(err, ns) {
   } else {
     let nsRegex = /^ns\d{1,2}\.wixdns\.net/; //match is good; no match means not our NS; test online at https://regex101.com/r/v0CyH3/1
     let nsCheck = '';
-    nsRegex.test(ns[0])? nsCheck = 'correctly set. ' : nsCheck = 'not correctly set. ';
+    nsRegex.test(ns[0])? nsCheck = 'set correctly. <br>' : nsCheck = 'not set correctly. <br>';
     responseJSON.response += `The domain's NS record is ${nsCheck}`;
     dns.resolve('www.'+event.result, 'CNAME', checkCNAME);
   }
@@ -74,7 +74,7 @@ function checkCNAME(err,cname){
   } else {
     let cnameRegex = /^www([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.wixdns\.net/; //match is good; no match means not our CNAME ; Stolen from https://github.com/wix-private/domain-troubleshooter-server/blob/8f3e69f721429744370c59252329df27f3583775/src/it/resources/domain-troubleshooter-server-config.xml 
     let cnameCheck = '';
-    cnameRegex.test(cname[0])? cnameCheck = 'correctly set. ' : cnameCheck = 'not correctly set. ';
+    cnameRegex.test(cname[0])? cnameCheck = 'set correctly. <br>' : cnameCheck = 'not set correctly. <br>';
     responseJSON.response += `The domain's CNAME record is ${cnameCheck}`;
     callback(null, responseJSON);
   }
