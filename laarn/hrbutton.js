de4d7d354becd758aab1abc6ea89543c4796446f
Michael Salaverry <michaels@wix.com>
2017-02-22 23:26:03 +0200

    haha game

---
+++ laarn/hrbutton.js
@@ -0,0 +1,45 @@
+//hr button
+//requirements
+var http = require('http')
+var url = require('url')
+
+var port = 3000
+var hrList = []
+var last = ''
+
+//Write an HTTP server
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'html'
+    })
+    var path = url.parse(request.url, true)
+    //console.log(path.pathname)
+    var api = path.pathname
+    
+    if(api!=='/favicon.ico'){
+        if(/^\/add/.test(api)){
+            hrList.push(api.slice(5))
+        }
+        if(api=='/reset'){
+            hrList=[];last='';
+            console.log('cleared')
+        }
+        if(/^\/remove/.test(api)){
+            hrList.splice(hrList.indexOf('api'))
+        }
+    }
+    //console.log(hrList.length)
+    
+    hrList.length>0?last = hrList.pop():'';
+    //console.log(last)
+    if(last!==''){
+        response.write(`Reported ${hrList.join(', ')}${hrList.length>0?', and '.concat(last):last} to HR`
+        )}
+    if(last!==''){
+        hrList.push(last)
+    }
+    //console.log(hrList)
+    //console.log(last)
+    response.end();
+});
+server.listen(port)
