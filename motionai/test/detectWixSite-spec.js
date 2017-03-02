475a3b667db1f274e0dee31157eaa1b834d0e8a5
Michael Salaverry <michaels@wix.com>
2017-03-02 11:36:10 +0200

    adding API

--- motionai/test/detectWixSite-spec.js
+++ motionai/test/detectWixSite-spec.js
@@ -1,21 +1,45 @@
 //Test detectWixSite.js to make sure changes don't break it
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var should = chai.should();
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
 
-describe('loads', ()=>{
-    it('should return a 200 response to our GET request')
+const querystring = require('querystring')
+
+const motionAiEndpoint = 'https://api.motion.ai/1.0/messageBot'
+
+const chatBotQueryParams = {
+    bot: 36231,
+    msg: '',
+    session: new Date.now(),
+    key: 'fd2e96e0e6f74353a8903226d6059102',
+    from: ''
+}
+
+var currQuery = chatBotQueryParams;
+
+function newQuery(msg){
+    var currQuery = chatBotQueryParams
+    currQuery.msg = msg
+    
+}
+
+chai.use(chaiHttp)
+
+describe('loads', () => {
+    it('should return a 200 response to our GET request', (done) => {
+        chai.request()
+    })
 })
-describe('URL parser', ()=>{
+describe('URL parser', () => {
     it('should not allow a bad input')
     it('should return a properly formatted URL from a normal URL')
 })
-describe('detect Wix site',()=>{
+describe('detect Wix site', () => {
     it('should detect NS connected testthiswix.com')
     it('should detect Free Site michaelsalaverry.wix.com/sandbox')
     it('should detect Pointed Domain')
 })
-describe('report not a Wix site'),()=>{
+describe('report not a Wix site'), () => {
     it('should fail on a non-wix site google.com')
     it('should fail when DNS correct but premium not paid for')
     it('should fail when A record wrong queendina.com')
