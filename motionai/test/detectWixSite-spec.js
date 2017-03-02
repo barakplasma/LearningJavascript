ab420b9233e517aa57dc8301297475de59e8a82d
Michael Salaverry <michaels@wix.com>
2017-03-02 11:00:08 +0200

    optimistic on the number of tests I can finish

--- motionai/test/detectWixSite-spec.js
+++ motionai/test/detectWixSite-spec.js
@@ -1,6 +1,7 @@
 //Test detectWixSite.js to make sure changes don't break it
-
-
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should();
 
 describe('loads', ()=>{
     it('should return a 200 response to our GET request')
@@ -10,7 +11,13 @@ describe('URL parser', ()=>{
     it('should return a properly formatted URL from a normal URL')
 })
 describe('detect Wix site',()=>{
-    it('should detect testthiswix.com')
-    it('should detect michaelsalaverry.wix.com/sandbox')
+    it('should detect NS connected testthiswix.com')
+    it('should detect Free Site michaelsalaverry.wix.com/sandbox')
+    it('should detect Pointed Domain')
 })
-describe('')
+describe('report not a Wix site'),()=>{
+    it('should fail on a non-wix site google.com')
+    it('should fail when DNS correct but premium not paid for')
+    it('should fail when A record wrong queendina.com')
+    it('should fail when CName wrong but A record correct')
+}
