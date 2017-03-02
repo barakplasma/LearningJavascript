7bf130e721abe3a17ddf3c03dbe053d462bb0f8a
Michael Salaverry <michaels@wix.com>
2017-03-02 11:47:27 +0200

    first test works woot

--- motionai/test/detectWixSite-spec.js
+++ motionai/test/detectWixSite-spec.js
@@ -1,6 +1,6 @@
 //Test detectWixSite.js to make sure changes don't break it
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require('chai')
+, chaihttp = require('chai-http');
 const should = chai.should();
 
 const querystring = require('querystring')
@@ -10,7 +10,7 @@ const motionAiEndpoint = 'https://api.motion.ai/1.0/messageBot'
 const chatBotQueryParams = {
     bot: 36231,
     msg: '',
-    session: new Date.now(),
+    session: Date.now(),
     key: 'fd2e96e0e6f74353a8903226d6059102',
     from: ''
 }
@@ -20,14 +20,16 @@ var currQuery = chatBotQueryParams;
 function newQuery(msg){
     var currQuery = chatBotQueryParams
     currQuery.msg = msg
-    
+    return motionAiEndpoint + '?' + querystring.stringify(currQuery)
 }
+//console.log(newQuery('hi'))
 
-chai.use(chaiHttp)
+chai.use(chaihttp)
 
 describe('loads', () => {
     it('should return a 200 response to our GET request', (done) => {
-        chai.request()
+        chai.request(newQuery('hi'))
+        done()
     })
 })
 describe('URL parser', () => {
@@ -39,9 +41,9 @@ describe('detect Wix site', () => {
     it('should detect Free Site michaelsalaverry.wix.com/sandbox')
     it('should detect Pointed Domain')
 })
-describe('report not a Wix site'), () => {
+describe('report not a Wix site', () => {
     it('should fail on a non-wix site google.com')
     it('should fail when DNS correct but premium not paid for')
     it('should fail when A record wrong queendina.com')
     it('should fail when CName wrong but A record correct')
-}
+})
