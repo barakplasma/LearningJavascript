d2b9042a4c4df000c300d7f85443d2bca5bfc66f
Michael Salaverry <michaels@wix.com>
2017-03-02 19:33:05 +0200

    fuck 'this'

--- motionai/test/detectWixSite-spec.js
+++ motionai/test/detectWixSite-spec.js
@@ -1,6 +1,6 @@
 //Test detectWixSite.js to make sure changes don't break it
-const chai = require('chai')
-, chaihttp = require('chai-http');
+const chai = require('chai'),
+    chaihttp = require('chai-http');
 const should = chai.should();
 
 const querystring = require('querystring')
@@ -17,6 +17,9 @@ const chatBotQueryParams = {
     from: ''
 }
 
+//matching statements
+const SantaDetected = 'We detected a Wix site at the URL you provided. This suggests you are experiencing a local issue'
+
 var currQuery = chatBotQueryParams;
 
 // CONSTRUCTOR FOR QUERIES SO I DONT HAVE TO KEEP TRACK OF SESSIONS BETWEEN TESTS
@@ -27,34 +30,36 @@ function Query() {
      * @param {String} m - message
      * @param {function} cb - callback
      */
-    this.m = function(m,cb){
+    this.m = function (m, cb) {
         this.currQuery.msg = m;
         //console.log(this.currQuery)
         this.n(cb)
     };
-    this.n = function(cb){
-        console.log(this.d()) //verifies that we are on the same session and the fxn runs
+    this.n = function (cb) {
+        //console.log(this.d()) //verifies that we are on the same session and the fxn runs
         chai.request(this.d())
-        .get('/')
-        .end(cb)
+            .get('/')
+            .end(cb)
     }
     this.currQuery.session = Date.now();
-    this.d = function(){return motionAiEndpoint + '?' + querystring.stringify(this.currQuery)}
+    this.d = function () {
+        return motionAiEndpoint + '?' + querystring.stringify(this.currQuery)
+    }
 }
 
 chai.use(chaihttp)
 
-describe('loads', function(){
-    it('should return a 200 response to our GET request', (done)=>{
+describe('loads', function () {
+    it('should return a 200 response to our GET request', (done) => {
         let q = new Query()
-        q.m('hi',(err,res)=>{
+        q.m('hi', (err, res) => {
             res.should.have.status(200);
             done()
         })
     })
-    it('should respond with our text', (done)=>{
+    it('should respond with our text', (done) => {
         let q = new Query()
-        q.m('hi',(err,res)=>{
+        q.m('hi', (err, res) => {
             //console.log(res.body)
             res.body.should.have.property('botResponse')
             res.body.botResponse.should.equal('Welcome to the Wix Technical Assistant.\nI\'m here to help you with technical issues.\n::next::\nAre you experiencing issues with your live site or the Wix Editor?')
@@ -62,38 +67,47 @@ describe('loads', function(){
         })
     })
 })
-describe('URL parser', function(){
-    this.timeout(15000);
+describe('URL parser', function () {
     it('should not allow a bad input')
     it('should return a properly formatted URL from a normal URL')
     it('should handle with www')
     it('should handle without www')
 })
-describe.skip('detect HTTPS Wix site', () => {
-    it('should detect NS connected testthiswix.com',(done)=>{
-        let q = new Query()
-        q.m('hi',(err,res)=>{
-            q.m('Live Site',(err,res)=>{
-                if(err)throw Error
-                q.m('testthiswix.com',(err,res)=>{
-                    if(err)throw Error
-                    console.log(res.body)
-                    done()
-                })
+
+function testSite(site,test,cb) {
+    let q = new Query()
+    q.m('hi', function(err, res){
+        if(err)throw Error
+        q.m('Live Site', function(err, res){
+            if (err) throw Error
+            q.m(site, function(err, res){
+                if (err) throw Error
+                //console.log(res.body)
+                test(res)
+                cb()
             })
         })
+    })
+}
+
+describe('detect HTTPS Wix site', function(){
+    this.timeout(10000);
+    it('should detect NS connected testthiswix.com', function(done){
+        testSite('testthiswix.com',function(res){
+            res.body.botResponse.should.equal(SantaDetected)
+        },done)
     })
     it('should detect Free Site michaelsalaverry.wix.com/sandbox')
     it('should detect Pointed Domain')
 })
 
-describe.skip('detect HTTP Wix site', () => {
+describe.skip('detect HTTP Wix site', function(){
     it('should detect NS connected RU site')
     it('should detect Free Site michaelsalaverry.wix.com/loadingtime')
     it('should detect Pointed Domain')
 })
 
-describe.skip('report not a Wix site', () => {
+describe.skip('report not a Wix site', function(){
     it('should fail on a non-wix site google.com')
     it('should fail when DNS correct but premium not paid for')
     it('should fail when A record wrong queendina.com')
