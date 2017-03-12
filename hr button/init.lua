74f8e3ebd18bc78610fbab671812ab32a0560d12
Michael Salaverry <michaels@wix.com>
2017-03-12 11:00:52 +0200

    works

--- hr button/init.lua
+++ hr button/init.lua
@@ -1,12 +1,16 @@
-lighton=0
-pin=4
-gpio.mode(pin,gpio.OUTPUT)
-tmr.alarm(1,2000,1,function()
-    if lighton==0 then
-        lighton=1
-        gpio.write(pin,gpio.HIGH)
-    else
-        lighton=0
-         gpio.write(pin,gpio.LOW)
-    end
+-- a simple HTTP server
+
+-- connect to WiFi access point
+wifi.setmode(wifi.STATION)
+wifi.sta.config("WIX Guest 2.4G", "Welcome2Wix!")
+wifi.sta.autoconnect(1)
+
+srv = net.createServer(net.TCP)
+srv:listen(80, function(conn)
+    conn:on("receive", function(sck, payload)
+        print(payload)
+        sck:send("HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n<h1> Hello from NodeMCU.</h1><p>Welcome to the server Michael</p>")
+    end)
+    conn:on("sent", function(sck) sck:close() end)
 end)
+
