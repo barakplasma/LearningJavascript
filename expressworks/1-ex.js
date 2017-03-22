a2d00a911dc384be1677b20532a675d37e60b92d
Michael Salaverry <michaels@wix.com>
2017-03-22 16:02:00 +0200

    passes ex 1 of express

--- expressworks/1-ex.js
+++ expressworks/1-ex.js
@@ -1,7 +1,7 @@
 var express = require('express')
 
 var app = express()
-app.get('/', function(req, res) {
+app.get('/home', function(req, res) {
     res.end('Hello World!')
 })
-app.listen(3000)
+app.listen(process.argv[2])
