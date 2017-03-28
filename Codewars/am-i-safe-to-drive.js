3aeba62527a200c03c096e5557806d1cb28588ca
Michael Salaverry <michaels@wix.com>
2017-03-28 19:49:58 +0300

    added a time handler function

--- Codewars/am-i-safe-to-drive.js
+++ Codewars/am-i-safe-to-drive.js
@@ -1,8 +1,11 @@
 module.exports = {
     drive: function(drinks, finished, drive_time) {
         function time(t) {
-            var x = 4;
+            var x = new Date;
+            //console.log(finished)
+            return x.setHours(finished.substr(0,2),finished.substr(3,2));
         }
+        //console.log(time(finished))
         return [1.0, false]
     }
 }
