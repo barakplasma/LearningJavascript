bb5fdfb313e6c6aef260aa37449d73e1e3498789
Michael Salaverry <michaels@wix.com>
2018-04-28 14:34:00 +0300

    passed all tests

--- Codewars/sum-of-intervals/kata.js
+++ Codewars/sum-of-intervals/kata.js
@@ -1,10 +1,11 @@
-const {range} = require('ramda');
+const {range, uniq, flatten, compose} = require('ramda');
 
 /**
  * Returns the sum of the length of all arrays passed to it minus overlaps
  * @param {Array.<number[]>} intervals 
  */
 module.exports.sumIntervals = function sumIntervals(intervals){
-    const newRange = range(intervals[0][0], intervals[0][1]);
-    return 4;
+    const allNumbersInAllIntervals = intervals.map(interval => range(interval[0], interval[1]));
+    const intervalRange = compose(uniq, flatten)(allNumbersInAllIntervals);
+    return intervalRange.length;
 }
